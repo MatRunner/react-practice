dde59f8288f50b6a8a7a8d14a3553af3536a2e76
yangyi <iyoung2018@126.com>
2022-08-08 22:01:20 +0800

    fix:小修一下

--- src/component/route.js
+++ src/component/route.js
@@ -1,16 +1,31 @@
 export const ROUTES = [
-  { label: '1', value: '', render: () => null, icon: '' },
-  { label: '2', value: '', render: () => null, icon: '' },
+  { label: '1', value: '', render: () => null, icon: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.sBtcnAhyj5x6lpfhmLLnxwHaHV?w=203&h=200&c=7&r=0&o=5&pid=1.7' },
+  { label: '2', value: '', render: () => null, icon: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.sBtcnAhyj5x6lpfhmLLnxwHaHV?w=203&h=200&c=7&r=0&o=5&pid=1.7' },
   {
     label: '3',
-    icon: '',
+    icon: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.sBtcnAhyj5x6lpfhmLLnxwHaHV?w=203&h=200&c=7&r=0&o=5&pid=1.7',
     children: [
       { label: '3-1', value: '' },
       {
         label: '3-2',
-        value: '' 
+        value: ''
       },
-    ], 
+    ],
   },
-  { label: '4', value: '', render: () => null, icon: '' },
+  { label: '4', value: '', render: () => null, icon: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.sBtcnAhyj5x6lpfhmLLnxwHaHV?w=203&h=200&c=7&r=0&o=5&pid=1.7' },
+  {
+    label: '5',
+    icon: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.sBtcnAhyj5x6lpfhmLLnxwHaHV?w=203&h=200&c=7&r=0&o=5&pid=1.7',
+    children: [
+      { label: '5-1', value: '' },
+      {
+        label: '5-2',
+        value: ''
+      },
+      {
+        label: '5-3',
+        value: ''
+      },
+    ],
+  },
 ]
